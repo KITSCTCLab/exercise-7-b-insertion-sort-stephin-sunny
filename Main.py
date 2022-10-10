431bcddd96b568d3083de28de3682aff0fbe2ee3
stephin-sunny <110010739+stephin-sunny@users.noreply.github.com>
2022-10-10 21:06:05 +0530

    7th exp b

--- Main.py
+++ Main.py
@@ -2,7 +2,14 @@ from typing import List
 
 def insertionSort(array) -> List[int]:
   # Write your code here
-
+  for x in range(1,len(array)):
+    selection = x
+    for y in range(x-1,-1,-1):
+      if (array[y] > array[selection]):
+        array[y],array[selection] = array[selection],array[y]
+        selection -=1
+  return array
+      
 # data = [9, 5, 1, 4, 3]
 input_data = input()
 data = []
